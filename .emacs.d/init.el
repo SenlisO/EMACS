36f64a4cd52baf09ae498fbc6a88116178689391
Richard Romick <gizmo.romick@gmail.com>
2021-03-19 06:13:51 -0400

    create new initialization file

---
+++ .emacs.d/init.el
@@ -0,0 +1,69 @@
+;; .emacs.d/init.el
+
+;; ===================================
+;; MELPA Package Support
+;; ===================================
+;; Enables basic packaging support
+(require 'package)
+
+;; Adds the Melpa archive to the list of available repositories
+;;(add-to-list 'package-archives
+;;           '("melpa" . "https://melpa.org/packages/") t)
+;;below is a mirror for melpa.  Use when the first doesn't work
+(add-to-list 'package-archives
+             '("melpa" . "http://www.mirrorservice.org/sites/melpa.org/packages/") t)
+
+
+;; Initializes the package infrastructure
+(package-initialize)
+
+;; If there are no archived package contents, refresh them
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Installs packages
+;;
+;; myPackages contains a list of package names
+(defvar myPackages
+  '(better-defaults                 ;; Set up some better Emacs defaults
+    elpy                            ;; Emacs Lisp Python Environment
+    material-theme                  ;; Theme
+    )
+  )
+
+;; Scans the list in myPackages
+;; If the package listed is not already installed, install it
+(mapc #'(lambda (package)
+          (unless (package-installed-p package)
+            (package-install package)))
+      myPackages)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (material-theme better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; ===================================
+;; Basic Customization
+;; ===================================
+
+(setq inhibit-startup-message t)    ;; Hide the startup message
+(load-theme 'material t)            ;; Load material theme
+(global-linum-mode t)               ;; Enable line numbers globally
+
+;; User-Defined init.el ends here
+
+;; ===================================
+;; Development setup
+;; ===================================
+;; Enable elpy
+(elpy-enable)
+
+;; User-Defined init.el ends here
